2c3ad1298a032e4bc4b1e680607216c9c0c1eb74
ru5s <hellperock@gmail.com>
2022-07-07 15:52:46 +0500

    Initial Commit

---
+++ sc_houmwork_module_20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sc_houmwork_module_20
+//
+//  Created by Ruslan Ismailov on 07/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
